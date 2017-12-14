76ebaf1d55e447026c5f39ffcdf6585d4f31e90b
Jayphen <jayphen@gmail.com>
2017-12-14 20:06:36 +0100

    Handle empty cart state

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -6,10 +6,6 @@ const getSize = (size, arr) => arr.find(item => item.short === size);
 const CartItem = (props) => {
   const item = { ...props.item };
 
-  const deleteItem = () => {
-    props.handleDelete(props.item.id);
-  };
-
   return (
     <div className="cart-item">
       <img className="cart-item--image" src={item.image} alt={item.name} />
@@ -24,8 +20,15 @@ const CartItem = (props) => {
           </span>
         </div>
         <div className="cart-item--actions">
-          <button className="btn btn--secondary"><span className="btn--secondary-underline">Change</span></button>
-          <button onClick={deleteItem} className="btn btn--secondary"><span className="btn--secondary-underline">Delete</span></button>
+          <button className="btn btn--secondary">
+            <span className="btn--secondary-underline">Change</span>
+          </button>
+          <button
+            onClick={() => props.handleDelete(item.id)}
+            className="btn btn--secondary"
+          >
+            <span className="btn--secondary-underline">Delete</span>
+          </button>
         </div>
       </div>
       <div className="cart-item--price">€{props.qty * item.price}</div>
--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -7,9 +7,12 @@ const getItem = id => products.find(product => product.id === id);
 
 const CartItems = ({ items, handleDelete }) => (
   <div>
-    {items.map(item => (
+    {items.length > 0 && items.map(item => (
       <CartItem key={item.id} handleDelete={handleDelete} size={item.size} qty={item.qty} item={getItem(item.id)} />
       ))}
+    {items.length === 0 && (
+      <div className="empty-cart">Cart is empty</div>
+    )}
   </div>
 );
 
